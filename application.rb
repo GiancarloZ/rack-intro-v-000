8ac55afab924fc83dfd94d0fe6b8bd1a1ad967bd
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-10-02 17:21:06 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World. My name is Giancarlo"
+    resp.write "Hello, World. My name is"
     resp.finish
   end
 
